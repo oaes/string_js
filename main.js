841dfaacabf15d32288b3e23754ea48504de3b20
Oaes Kuruni <oaeskuruni27@gmail.com>
2019-07-22 23:53:59 +0600

    added a file

---
+++ main.js
@@ -0,0 +1,18 @@
+//String methods in js
+
+var a = 'i am'
+var b = 'AKASH'
+console.log(a > b)
+//for change small to uppercase
+console.log(a.toUpperCase())
+//for change small to lowercase
+console.log(b.toLowerCase())
+//for counting the string number
+console.log(a.charAt(3))
+console.log(b.charAt(2))
+
+//we can justify which are start with,and it return us boolean value.
+console.log(a.startsWith('i am'))
+
+//we can justify which are end with,and it return us boolean value.
+console.log(b.endsWith('AKASH'))
